e6c795f73eb1a6013111dc14d3718f54155527ba
Aaron Harrington <aharrington@aviatainc.com>
2015-05-12 18:12:14 -0600

    updated

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
 FROM aviata/java7
 
+ENV REDIS_IP localhost
+ENV POSTGRES_IP localhost
+
 COPY start_service.sh /home/
 RUN chmod +x /home/start_service.sh
 
 
-CMD sh /home/start_service.sh
+CMD sh /home/start_service.sh $HOST_IP
--- start_service.sh
+++ start_service.sh
@@ -1,4 +1,4 @@
 #!bin/sh
+set -x
 
-rm /mnt/RUNNING_PID
-/mnt/bin/nda-services
+/mnt/bin/nda-services -Ddb.postgres.url="jdbc:postgresql://${POSTGRES_IP}:5432/campaign_builder" -Ddb.postgres.host=$POSTGRES_IP -Dredis.host=$REDIS_IP
