b9a4da5ebefe73b1d5be9288cbfb95033f97832b
Aaron Harrington <aharrington@aviatainc.com>
2015-05-13 15:35:07 -0600

    this docker should be able to use arbitary ports and IPs now

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
-FROM aviata/java7
+FROM java:8
 
 ENV REDIS_IP localhost
 ENV POSTGRES_IP localhost
+ENV REDIS_PORT 6379
+ENV POSTGRES_PORT 5432
 
 COPY start_service.sh /home/
 RUN chmod +x /home/start_service.sh
--- start_service.sh
+++ start_service.sh
@@ -1,4 +1,6 @@
 #!bin/sh
 set -x
 
-/mnt/bin/nda-services -Ddb.postgres.url="jdbc:postgresql://${POSTGRES_IP}:5432/campaign_builder" -Ddb.postgres.host=$POSTGRES_IP -Dredis.host=$REDIS_IP
+rm /mnt/RUNNING_PID
+
+/mnt/bin/nda-services -Ddb.postgres.url="jdbc:postgresql://${POSTGRES_IP}:${POSTGRES_PORT}/campaign_builder" -Ddb.postgres.host=$POSTGRES_IP -Dredis.host=$REDIS_IP -Ddb.postgres.port=$POSTGRES_PORT -Dredis.port=$REDIS_PORT
